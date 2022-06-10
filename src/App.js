589904654e3e972fe0faebe348da7800afda318c
mdkawsar-rachel <79735880+mdkawsar-rachel@users.noreply.github.com>
2022-06-10 13:16:41 +0000

    Router add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import {Typography, Table,TableRow, TableCell, tableCellClasses, AppBar, CssBaseline , Toolbar, Container} from '@mui/material'
 import BasicTabs from './Tab';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import about from './pages/about';
 
 const App = () => {
 
   return (
     <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/about' element={<about/>}/>
+      </Routes>
+    </BrowserRouter>
       <CssBaseline/>
       <AppBar position='relative' elevation={0}>
       <Toolbar>
