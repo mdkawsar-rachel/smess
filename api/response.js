136250cb6a5abb57300ca8782f6dc8393a7e29b9
mdkawsar-rachel <79735880+mdkawsar-rachel@users.noreply.github.com>
2022-06-11 13:52:18 +0000

    Api Mongodb test

--- api/response.js
+++ api/response.js
@@ -6,8 +6,20 @@ export default function handler(request, response) {
   mongoose.connect("mongodb+srv://kawsar:n0EBot0wdLGh5YBO@atlascluster.xzdzk.mongodb.net/smess?retryWrites=true&w=majority")
 .then(() => {
   console.log("Connected to mongodb");
+
+  mongoose.connection.on('open', function (ref) {
+    console.log('Connected to mongo server.');
+    //trying to get collection names
+    mongoose.connection.db.listCollections().toArray(function (err, names) {
+        console.log(names); // [{ name: 'dbname.myCollection' }]
+        //module.exports.Collection = names;
+        tables = JSON.stringify(names);
+    });
+})
+
   response.status(200).json({
-    msg: "Hello"
+    msg: "Hello",
+    tables: tables
   });
 })
 .catch((err) =>{
